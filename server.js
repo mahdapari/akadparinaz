173788fa1733d086f93d7ef26b6328aec84f9296
mahdapari <ho3einnia1@gmail.com>
2025-04-28 17:27:19 +0330

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
+
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
+});
